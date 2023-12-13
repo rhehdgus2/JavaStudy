e5fd30e510744162172ff5781b421d355e0703b9
rhehdgus2 <kdgkdg0217@naver.com>
2023-12-13 16:49:23 +0900

    테스트

---
+++ src/Example3_1.java
@@ -0,0 +1,11 @@
+public class Example3_1 {
+	public static void main(String[] args) {
+		int x, y;
+		
+		x = y = 3; // y에 3이 저장된 후에, x에 3이 저장됨
+		System.out.println("x=" +x);
+		System.out.println("y=" +y);
+	}
+}
+
+
---
+++ src/Example5_4.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Example5_4 {
+	public static void main(String[] args) {
+		int[] numArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};  // 초기 정수 배열 선언 및 초기화
+		System.out.println(Arrays.toString(numArr));  // 배열의 현재 상태 출력
+		
+		for(int i = 0; i < 100; i++) {
+			int n = (int)(Math.random() * 10);  // 0 ~ 9 중의 한 값을 임의로 얻는다. 3을 얻어다고 가정하자.
+			// numArr[0]과 numArr[n]의 값을 서로 바꾼다.
+			int tmp = numArr[0];  // numArr[0]의 값을 변수 tmp에 저장
+			numArr[0] = numArr[n];  // numArr[3]의 값을 numArr[0]에 저장
+			numArr[n] = tmp;  // tmp의 값을 numArr[3]에 저장
+		}
+		System.out.println(Arrays.toString(numArr));  // 배열의 현재 상태 출력
+	}  // main의 끝
+}
