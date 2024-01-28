60e2c8eb70a5cb51378a8a5512e00fbe72cd12a7
rhehdgus2 <kdgkdg0217@naver.com>
2024-01-28 21:16:05 +0900

    7장

---
+++ src/Chapter7/Example7_1.java
@@ -0,0 +1,45 @@
+package Chapter7;
+
+//TV 클래스 정의
+class Tv {
+ boolean power;      // 전원 상태(on / off)
+ int channel;        // 채널
+
+ void power() {
+     power = !power;  // 전원을 켜거나 끄는 메서드
+ }
+
+ void channelUp() {
+     ++channel;       // 채널을 증가시키는 메서드
+ }
+
+ void channelDown() {
+     --channel;       // 채널을 감소시키는 메서드
+ }
+}
+
+//SmartTv는 Tv를 상속하는 클래스
+class SmartTv extends Tv {
+ boolean caption;    // 캡션 상태(on / off)
+
+ void displayCaption(String text) {
+     if (caption) {   // 캡션 상태가 켜져 있을 때만 특정 텍스트를 출력하는 메서드
+         System.out.println(text);
+     }
+ }
+}
+
+//메인 클래스
+public class Example7_1 {
+ public static void main(String[] args) {
+     SmartTv stv = new SmartTv();   // SmartTv 인스턴스 생성
+
+     stv.channel = 10;              // Tv 클래스로부터 상속받은 멤버 변수 조작
+     stv.channelUp();               // Tv 클래스로부터 상속받은 메서드 호출
+     System.out.println(stv.channel);
+
+     stv.displayCaption("Hello, World");  // SmartTv의 추가된 메서드 호출
+     stv.caption = true;                   // SmartTv의 멤버 변수 조작
+     stv.displayCaption("Hello, World");  // SmartTv의 추가된 메서드 호출
+ }
+}
---
+++ src/Chapter7/Example7_13.java
@@ -0,0 +1,33 @@
+package Chapter7;
+
+class Outer {
+    private int outerIv = 0;
+    static int outerCv = 0;
+
+    // 인스턴스 멤버 클래스
+    class InstanceInner {
+        int iiv = outerIv;  // 외부 클래스의 private 멤버도 접근 가능
+        int iiv2 = outerCv;
+    }
+
+    // 스태틱 멤버 클래스
+    static class StaticInner {
+// 스태틱 클래스는 외부 클래스의 인스턴스 멤버에 직접 접근할 수 없다.
+// 		int siv = outerIv;  // 에러
+        static int scv = outerCv;
+    }
+
+    void myMethod() {
+        int lv = 0;
+        final int LV = 0;  // JDK 1.8부터 final 생략 가능
+
+        // 지역 내부 클래스
+        class LocalInner {
+            int liv = outerIv;
+            int liv2 = outerCv;
+// 외부 클래스의 지역 변수는 final이 붙은 변수(상수)만 접근 가능
+// 			int liv3 = lv;  // 에러(JDK1.8부터 에러 아님)
+            int liv4 = LV;
+        }
+    }
+}
